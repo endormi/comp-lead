70b188bbfc2442ff4ec7ff9f97d205be5fa89c5f
endormi <ernosalo14@gmail.com>
2019-09-28 03:35:33 +0300

    Update setup.py
    * Remove find_packages exclude
    
    * Remove include_package_data true
    
    * Update python_requires

--- setup.py
+++ setup.py
@@ -12,12 +12,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/endormi/comp-lead",
-    packages=setuptools.find_packages(exclude=("tests",)),
-    include_package_data=True,
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
